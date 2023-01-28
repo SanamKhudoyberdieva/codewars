79bc51fd2df628862d2effe868d1eb9ca474a17e
Sanam Khudoyberdieva <khudoyberdievasanamkhon@gmail.com>
2023-01-28 14:28:03 +0500

    Even or Odd

---
+++ 5-even-or-odd.js
@@ -0,0 +1,13 @@
+function evenOrOdd(number) {
+    if (number % 2 === 0){
+        return "Even";
+    } else {
+        return "Odd";
+    }
+};
+
+
+
+function even_or_odd(number) {
+    return number % 2 ? "Odd" : "Even"
+}

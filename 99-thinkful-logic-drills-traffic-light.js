bbcd73dccfb92c2e75f16ee2142f16338da6197b
Sanam Khudoyberdieva <khudoyberdievasanamkhon@gmail.com>
2023-01-29 18:09:02 +0500

    Convert a Number to a String!

---
+++ 99-thinkful-logic-drills-traffic-light.js
@@ -0,0 +1,25 @@
+// The first code I wrote
+function updateLight(current) {
+    if (updateLight("green") == 1) {
+        return updateLight("yellow")
+    } else if (updateLight("yellow" == 1)) {
+        return updateLight("red")
+    } else {
+        return updateLight("green")
+    }
+}
+
+
+// When I searched
+function updateLight(current) {
+    if (current == "green") {
+        return "yellow"
+    } else if (current == "yellow") {
+        return "red"
+    } else {
+        return "green"
+    }
+}
+
+
+

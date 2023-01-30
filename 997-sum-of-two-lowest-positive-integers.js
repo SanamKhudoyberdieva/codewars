af073dc49c4130034b266ba1d4273cad3243e9ac
Sanam Khudoyberdieva <khudoyberdievasanamkhon@gmail.com>
2023-01-30 18:07:33 +0500

    Sum of two lowest positive integers

---
+++ 997-sum-of-two-lowest-positive-integers.js
@@ -0,0 +1,7 @@
+function sumTwoSmallestNumbers(numbers) {
+    let sum = 0;
+    let result = numbers.sort((a, b) => a - b)
+    return sum = result[0] + result[1]
+}
+
+// .sort() ichidagini yozmagan ekanman 

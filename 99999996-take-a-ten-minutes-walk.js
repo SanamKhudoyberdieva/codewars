3205ee6c20a427398f06a4cadd11919c6706683f
Sanam Khudoyberdieva <khudoyberdievasanamkhon@gmail.com>
2023-02-09 18:11:22 +0500

    Take a Ten Minutes Walk

---
+++ 99999996-take-a-ten-minutes-walk.js
@@ -0,0 +1,13 @@
+function isValidWalk(walk) {
+    const north = walk.filter(item => { return item === "n" }).length;
+    const south = walk.filter(item => { return item === "s" }).length;
+    const east = walk.filter(item => { return item === "e" }).length;
+    const west = walk.filter(item => { return item === "w" }).length;
+    
+    return walk.length === 10 && north === south && east === west;
+}
+
+
+function isValidWalk(walk) {
+    return walk.length === 10  || walk.length === 5 ? true : false
+}

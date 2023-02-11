789f91ab688b315d528628b4bce7cbf444633cd4
Sanam Khudoyberdieva <khudoyberdievasanamkhon@gmail.com>
2023-02-11 17:00:22 +0500

    Welcome!

---
+++ 999999995-welcome.js
@@ -0,0 +1,27 @@
+function greet(language) {
+	let welcome = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+    }
+    for(let i in welcome){
+        if(i == language){
+            return welcome[i]
+        }
+    }
+    return welcome.english
+}
